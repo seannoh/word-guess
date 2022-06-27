4e455e1909403dd6b2212652a2acaf29a2087248
hmanjun <harshith.hm03@gmail.com>
2022-06-27 14:11:43 -0700

    Add wordDisplay

--- assets/script.js
+++ assets/script.js
@@ -5,7 +5,18 @@ var newWordBtn = document.getElementById("new-word-btn")
 
 var posWords = ["computer","test","people","phone","mouse"]
 var hiddenWord = "                           "
+var currentGuess = ""
+var guessedLetters = []
 
 function chooseWord(){
     var hiddenWord = posWords[Math.floor(Math.random() * (posWords.length - 1))]
 }
+
+function displayWord(){
+    var showString = ""
+    for(var i = 0; i < currentGuess.length; i++){
+        showString += currentGuess.charAt(i) + " "
+    }
+    hiddenWord.textContent = showString
+}
+
