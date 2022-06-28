651dde24a32d0c0ee85d0243b20083f160f10d10
Sean Oh <seanoh@ucsb.edu>
2022-06-27 18:15:10 -0700

    fixed multiple timers bug and refactored

--- assets/script.js
+++ assets/script.js
@@ -1,17 +1,45 @@
+// variables for DOM elements
 var scoreBoard = document.getElementById("score")
 var timer = document.getElementById("timer")
 var hiddenText = document.getElementById("hidden-word")
 var newWordBtn = document.getElementById("new-word-btn")
 var endGameMsg = document.getElementById("end-game-msg")
 
-var posWords = ["computer","test","people","phone","mouse"]
-var hiddenWord = "password"
-var currentGuess = ""
-var guessedLetters = []
-var secondsLeft = 10
-var winCount = 0
-var lossCount = 0
-var gameOver = false
+// local js variables
+var posWords = ["computer","test","people","phone","mouse"]  // array of possible words
+var hiddenWord = "password"  // current word chosen
+var currentGuess = ""     // string of current guess 
+var guessedLetters = []   // array of guessed letters
+var secondsLeft = 10      // seconds left in timer
+var winCount = 0          // number of wins
+var lossCount = 0         // number of losses
+var gameOver = true      // bool to keep track of when the game is over
+
+// FUNCTIONS
+
+function initGame(){
+  if(!gameOver) return
+  gameOver = false
+  guessedLetters = []
+  secondsLeft = 10
+  endGameMsg.style.display = "none";
+  console.log(endGameMsg);
+  chooseWord()
+  initTimer()
+}
+
+function initTimer(){
+  timer.textContent = secondsLeft
+  var timerInterval = setInterval(function() {
+      if(secondsLeft === 0 || gameOver){
+          clearInterval(timerInterval)
+          endGame();
+      } else {
+        secondsLeft--
+        timer.textContent = secondsLeft
+      }
+  }, 1000)
+}
 
 function chooseWord(){
     hiddenWord = posWords[Math.floor(Math.random() * (posWords.length - 1))]
@@ -40,47 +68,27 @@ function updateGuess(){
     displayWord();
 }
 
-document.addEventListener("keypress", function(event) {
-    if(gameOver==false){
-        var keyPressed = event.key.toLowerCase();
-        if(!(guessedLetters.includes(keyPressed))) {
-            guessedLetters.push(keyPressed);
-        }  
-        updateGuess();
-    }
-})
-
-newWordBtn.addEventListener("click",function(){
-    gameOver = false
-    guessedLetters = []
-    secondsLeft = 10
-    endGameMsg.style.display = "none";
-    chooseWord()
-    initTimer()
-})
-
-function initTimer(){
-    var timerInterval = setInterval(function() {
-        if(secondsLeft > 0){
-            secondsLeft--
-            timer.textContent = secondsLeft
-        } else {
-            clearInterval(timerInterval)
-            endGame();
-        }
-    }, 1000)
+function handleKeyPress(event) {
+  if(gameOver==false){
+      var keyPressed = event.key.toLowerCase();
+      if(!(guessedLetters.includes(keyPressed))) {
+          guessedLetters.push(keyPressed);
+      }  
+      updateGuess();
+  }
 }
 
 function endGame() {
-    endGameMsg.style.display = "block";
     if(currentGuess == hiddenWord && secondsLeft > 0){
         winCount++
         gameOver = true
+        endGameMsg.style.display = "block";
         endGameMsg.textContent = "You Win"
         scoreUpdate()
     } else if(currentGuess != hiddenWord && secondsLeft === 0){
         lossCount++
         gameOver = true
+        endGameMsg.style.display = "block";
         endGameMsg.textContent = "You Lose"
         scoreUpdate()
     }
@@ -100,3 +108,6 @@ function displayScore(){
 }
 
 displayScore()
+
+newWordBtn.addEventListener("click",initGame)
+document.addEventListener("keypress", handleKeyPress)
